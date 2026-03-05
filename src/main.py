fafd965a4b57fa3d439216df32160327a0c720b1
mo3ve <219550195+fladischer-tristan@users.noreply.github.com>
2026-03-05 15:10:48 +0100

    rebase

--- src/main.py
+++ src/main.py
@@ -2,7 +2,26 @@
 Entry point for game
 """
 
-import Tetris as t # Whole game as a package
+#import Tetris as t # Whole game as a package
 
-t.run()
+#t.run()
 
+
+
+class Car:
+    def __init__(self):
+        self.position = 0
+
+    def fahren(self, distance: int):
+        self.position = self.position + distance
+
+myAuto = Car()
+print(f"Auto1 pos: {myAuto.position}") # 0
+myAuto.fahren(130)
+print(f"Auto1 pos: {myAuto.position}") # 130
+
+myAuto2 = Car()
+print(f"Auto2 pos: {myAuto2.position}") # 0
+for i in range(10):
+    myAuto2.fahren(1)
+print(f"Auto2 pos: {myAuto2.position}") # 10
